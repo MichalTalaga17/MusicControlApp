1217402871efabcb0c001ba8a1e2ba40aa12f5bb
Michał Talaga <michal.talaga.programming@gmail.com>
2024-11-14 15:22:45 +0100

    Initial Commit

---
+++ StandBy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StandBy
+//
+//  Created by Michał Talaga on 14/11/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StandByTests/StandByTests.swift
@@ -0,0 +1,17 @@
+//
+//  StandByTests.swift
+//  StandByTests
+//
+//  Created by Michał Talaga on 14/11/2024.
+//
+
+import Testing
+@testable import StandBy
+
+struct StandByTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
